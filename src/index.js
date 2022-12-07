0180f5020b9d22f5e94b3f3e50fa9b807cece2da
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-07 09:35:06 +0000

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+//Add main event listener as soon as DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  docForm = document.querySelector("form");
+  docForm.addEventListener("submit", submitListener);
 });
+
+function submitListener(evt){
+  evt.preventDefault();
+  const newItem = evt.target.querySelector("#new-task-description").value;
+  addItemToList(newItem);
+  evt.target.reset();
+}
+
+function addItemToList(newItem){
+  //Obtain reference to task DOM element
+  const taskViewElement = document.querySelector("#tasks");
+  //List element activities
+  const newTaskElement = document.createElement("li")
+  newTaskElement.textContent = `${newItem} `;  
+  //List element child button activities
+  const newTaskElementButton = document.createElement("button")
+  newTaskElementButton.textContent = 'X'
+  newTaskElementButton.addEventListener('click', deleteTaskItem)
+  //Add the new DOM node as a child the task view element
+  taskViewElement.appendChild(newTaskElement);
+  newTaskElement.appendChild(newTaskElementButton);
+}
+
+function deleteTaskItem(evt){
+  evt.target.parentNode.remove()
+}
